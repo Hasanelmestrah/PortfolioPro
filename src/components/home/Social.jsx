664bdb0475d5292121d804c95018ddcd74fb34a5
hasanelmestrah <hasanelmestrah@gmail.com>
2023-12-19 19:56:08 +0200

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/hasanelmestrah"
+        className="home__social-icon"
+        target="_blank">
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.twitter.com/hasanelmestrah"
+        className="home__social-icon"
+        target="_blank">
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/hasanelmestrah"
+        className="home__social-icon"
+        target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
