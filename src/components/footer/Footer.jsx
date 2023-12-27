4d70d2beeb284bb4bb5deb3ca65ab3dc4ba0e540
hasanelmestrah <hasanelmestrah@gmail.com>
2023-12-27 20:44:51 +0200

    update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Hasan</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/hasanelmestrah"
+            className="home__social-icon"
+            target="_blank">
+            <i class="uil uil-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.twitter.com/hasanelmestrah"
+            className="home__social-icon"
+            target="_blank">
+            <i class="uil uil-twitter-alt"></i>
+          </a>
+
+          <a
+            href="https://github.com/hasanelmestrah"
+            className="home__social-icon"
+            target="_blank">
+            <i class="uil uil-github"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
